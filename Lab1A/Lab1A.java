70d72f0f80fa5963d955eec0f12ce7d1c928f221
Andreas Magnusson <andreas.magnusson90@gmail.com>
2016-11-08 16:04:37 +0100

    Ran test program and fixed bugs

--- Lab1A/Lab1A.java
+++ Lab1A/Lab1A.java
@@ -5,15 +5,8 @@ public class Lab1A {
 	
 	public static void main(String[] args) throws FileNotFoundException {
 
-		assert args.length == 2;
-
-		if (args.length != 2) {
-			System.out.println("Not enough argumentsPlease run as \"java Lab1A <filename> <element>\". Exits...");
-			System.exit(0);
-		}
-
-		String filename = args[0];
-		int element = Integer.parseInt(args[1]);
+		int element = Integer.parseInt(args[0]);
+		String filename = args[1];
 
 		File file = new File(filename);
 		Scanner scan = new Scanner(file);
