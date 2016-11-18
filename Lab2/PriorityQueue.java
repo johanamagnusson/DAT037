350da8d615ed3aeb7798c3615f34cf4a3582994b
Carl Smedstad <carl.smedstad@gmail.com>
2016-11-18 14:03:31 +0100

    Finished bubbleUp method.

--- Lab2/PriorityQueue.java
+++ Lab2/PriorityQueue.java
@@ -4,7 +4,7 @@
 public class PriorityQueue<E extends Comparable<? super E>> {
 
     private ArrayList<E> a;
-    private int sign;
+    private final int sign;
     private int size;
     
     public PriorityQueue(ArrayList<E> a, Comparator<? super E> comp, int sign) {
@@ -29,20 +29,43 @@ public class PriorityQueue<E extends Comparable<? super E>> {
     }
     */
 
+    private void swap(int i1, int i2) {
+        E tmp = a[i1];
+        a[i1] = a[i2];
+        a[i2] = tmp;
+    }
+
+    private bool hasParent(int i) {
+        return i != 0;
+    }
+
+    /*
     private int parentIndex(int i) {
         return i/2;
     }
+    */
+    
+    private int leftChildIndex(int i) {
+        return 2 * i + 1;
+    }
 
-    
-    
+    private int rightChildIndex(int i) {
+        return 2 * i + 2;
+    }
+
     private void bubbleUp() {
-
-        
-        
+        int index = this.size - 1;
+        int parentIndex;
+        while(hasParent(index)) {
+            parentIndex = index / 2;
+            if(sign * a[index].compareTo(a[parentIndex]) > 0) {
+                swap(index, parentIndex);
+                index = parentIndex;
+            }
+        }
     }
 
     private void bubbleDown() {
-
         
         
     }
