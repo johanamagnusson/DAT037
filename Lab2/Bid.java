b2a92f84fc22e373a1bfcb186c05b2fbc81d415b
Carl Smedstad <carl.smedstad@gmail.com>
2016-11-21 14:55:38 +0100

    Finished Bid class

--- Lab2/Bid.java
+++ Lab2/Bid.java
@@ -1,12 +1,42 @@
-
 public class Bid {
 
+    private String name;
+    private String op;
+    private int value;
+    private int oldValue;
+    private int newValue;
     
     public void Bid(String name, String op, int value) {
-        
+        this.name = name;
+        this.op = op;
+        this.value = value;
     }
-
+    
     public void Bid(String name, String op, int oldValue, int newValue) {
-
+        this.name = name;
+        this.op = op;
+        this.oldValue = oldValue;
+        this.newValue = newValue;
     }
+    
+    public String getName(Bid b) {
+        return name;
+    }
+
+    public String getOp(Bid b) {
+        return op;
+    }
+
+    public int getValue(Bid b) {
+        return value;
+    }
+
+    public int getOldValue(Bid b) {
+        return oldValue;
+    }
+
+    public int getNewValue(Bid b) {
+        return newValue;
+    }
+    
 }
