c85d8a6a23e650de9634126b5a3c56819566b2fa
Carl Smedstad <carl.smedstad@gmail.com>
2016-11-24 16:22:19 +0100

    IO test in Lab3

---
+++ Lab3/src/DijkstraPath.java
@@ -0,0 +1,25 @@
+public class DijkstraPath implements Path {
+
+    private ArrayList<E> path;
+    
+    public DijkstraPath(List<BStop> nodeList, List<BLineTable> nodeLines) {
+
+    }
+
+    public void computePath(E from, E to) {
+        
+        
+        
+        
+        
+    }
+
+    public Iterator<E> getPath() {
+        return path.iterator();
+    }
+
+    public int getPathLength() {
+        
+    }
+    
+}
--- Lab3/src/Lab3.java
+++ Lab3/src/Lab3.java
@@ -1,9 +1,31 @@
-import org.Lab3Help.*;
+// Compile: javac -cp Lab3Help.jar Lab3.java
+// Run:     java -cp Lab3Help.jar:. Lab3
+import Lab3Help.*;
+import java.util.*;
+import java.io.*;
 
 public class Lab3 {
 
-    public static void main(String[] args) {
-        System.out.println("Test");
+
+    
+    public static void main(String[] args) throws IOException {
+
+        Lab3File fileReader = new Lab3File();
+        List<BStop> nodeList = new ArrayList<BStop>();
+        List<BLineTable> lineTable = new ArrayList<BLineTable>();
+        try {
+            nodeList = fileReader.readStops(args[0]);
+            lineTable = fileReader.readLines(args[1]);
+        } catch(MalformedData md) {
+            System.out.println("MalformedData exception.");
+        }
+        String startNode = args[2];
+        String stopNode = args[3];
+
+        for(int i = 0; i < nodeList.size(); i++) {
+            System.out.println(nodeList.get(i).getName());
+        }
+        
     }
     
 }
