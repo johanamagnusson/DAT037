0a34f20ad1e7369bb18985495c9f953f8918d6b6
Andreas Magnusson <andreas.magnusson90@gmail.com>
2016-11-28 00:15:13 +0100

    Started sketching Dijkstra

--- Lab3/src/DijkstraPath.java
+++ Lab3/src/DijkstraPath.java
@@ -1,15 +1,22 @@
 import Lab3Help.*;
+import java.util.*;
 
 public class DijkstraPath implements Path {
 
     private ArrayList<E> path;
+    private Graph graph;
     
     public DijkstraPath(List<BStop> nodeList, List<BLineTable> nodeLines) {
-        
+        path = new ArrayList<E>();
+        graph = new Graph(nodeList, BLineTable);
     }
 
     public void computePath(E from, E to) {
         
+        int[] d = new int[this.graph.getSize()];
+        E[] p = (E[]) new Object[this.graph.getSize()];
+
+
     }
 
     public Iterator<E> getPath() {
