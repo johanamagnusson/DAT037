0aa55c186474972045fd38d2f08df8497636393d
Andreas Magnusson <andreas.magnusson90@gmail.com>
2016-12-01 01:06:04 +0100

    Created DijsktraStringPath

---
+++ Lab3/src/DijsktraStringPath.java
@@ -0,0 +1,25 @@
+import Lab3Help.*;
+import java.util.*;
+import java.io.*;
+
+public class DijkstraStringPath implements Path<String> {
+
+    private DijkstraPath<String> dPath;
+
+    public DijstraStringPath(List<BStop> nodeList, List<BLineTable> nodeLines) {
+        this.dPath = new DijkstraPath<String>(nodeList, nodeLines);
+    }
+
+    public void computePath(String from, String to) {
+        this.dPath.computePath(from, to);
+    }
+
+    public Iterator<String> getPath() {
+        return this.dPath.getPath();
+    }
+
+    public int getPathLength() {
+        return this.dPath.getPathLength();
+    }
+}
+
